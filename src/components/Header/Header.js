1df9213800b75639b4de48c1d6e414f032dd3bfe
Duc <ducdz2001tnt@gmail.com>
2023-03-16 23:48:47 +0700

    movies

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import user from "../../images/user.png";
+import "./Header.scss";
+
+function Header() {
+  return (
+    <div className="header">
+      <Link to={"/"} style={{ textDecoration: "none" }}>
+        <div className="logo">Movie App</div>
+      </Link>
+      <div className="user-image">
+        <img src={user} alt="user" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import MovieListing from "../MovieListing/MovieListing";
+import movieApi from "../../common/Api/movieApi";
+import { APIKey } from "../../common/Api/MovieApiKey";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/movieSlice";
+
+const Home = () => {
+  const dispatch = useDispatch();
+  const movieText = "Harry";
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await movieApi
+        .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+        .catch((err) => console.log(err));
+      dispatch(addMovies(response.data));
+    };
+    fetchMovies();
+  }, []);
+
+  return (
+    <div>
+      <div className="banner-img"></div>
+      <MovieListing />
+    </div>
+  );
+};
+
+export default Home;
